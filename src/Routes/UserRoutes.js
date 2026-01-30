a5b35c0a0557f2a205d0250b414d3cf39913b384
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-01-30 22:43:13 +0530

    File Push

---
+++ src/Routes/UserRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+
+// Controllers
+const { addChar, getChar, delChar, updateChar, getSingleChar } = require("../Controllers/UserControllers")
+
+
+router.route("/users")
+    .post(addChar)
+    .get(getChar)
+
+// With Params
+router.route("/users/:id")
+    .put(updateChar)
+    .delete(delChar)
+    .get(getSingleChar)
+
+module.exports = router;
+
