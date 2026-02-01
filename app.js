eb83e9c05b79b740e2484abe13ade05afbd83055
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-02-01 10:27:30 +0530

    Making For Production

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const cors = require("cors")
 const dotenv = require("dotenv").config()
 
 // Middlewares
-const ErrorMiddlware = require("./Middlewares/Error")
+const ErrorMiddlware = require("./src/Middlewares/Error.js")
 
 const app = express()
 
-app.use(cors())
 
-// Data Passing And Receving
+// Data Passing And Receving & Default Middleware
+app.use(cors({ origin: "*" }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 // Routes Import
-const UserRoutes = require("./Routes/UserRoutes")
+const UserRoutes = require("./src/Routes/UserRoutes.js")
 // DB Import
-const ConnectDB = require("./DBConfig/DbConfig")
+const ConnectDB = require("./src/DBConfig/DbConfig.js")
 
 // Routes
 app.use("/api/v1", UserRoutes)
@@ -31,6 +31,12 @@ ConnectDB()
 
 app.use(ErrorMiddlware);
 
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Profile Manager Server Started"
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server Running At The Port : http://localhost:${process.env.PORT}`)
